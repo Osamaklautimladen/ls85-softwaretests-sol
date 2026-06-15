b1ef19a7b74c1377c48439a63e16df750edebb47
lacksaufeventer <hibbelnstefan@gmail.com>
2026-06-15 09:06:57 +0200

    Kap1 Aufgabe 1 gelöst

--- 01_grundlagen/code/starter.py
+++ 01_grundlagen/code/starter.py
@@ -23,35 +23,35 @@ def berechne_rabatt(preis: float, prozent: float) -> float:
 # Aufgabe 1a): Beantworte folgende Fragen als Kommentar:
 
 # Error (falsche Handlung des Entwicklers):
-# TODO: Deine Antwort hier
+# Ein Logikfehler ist dem Entwickler unterlaufen
 
 # Defect (fehlerhafte Stelle im Code):
-# TODO: Deine Antwort hier
+# Rabatt wird falsch berechnet
 
 # Failure (was der Benutzer bemerken würde):
-# TODO: Deine Antwort hier
+# Der Wert, der zurückgegeben wird, ist viel zu groß.
 
 
 # Aufgabe 1b): Korrigiere die Funktion unten und füge print()-Tests hinzu.
 
 def berechne_rabatt_korrigiert(preis: float, prozent: float) -> float:
-    """
-    Korrigierte Version von berechne_rabatt().
-    TODO: Implementiere die korrekte Logik.
-    """
-    pass  # TODO: Ersetze 'pass' durch deine Implementierung
-
+    if prozent<0.0:
+        return "Prozent darf nicht negativ sein."
+    elif preis<0.0:
+        return "preis darf nicht negativ sein."
+    elif prozent >100:
+        return "Prozent muss unter 100 sein."
+    rabatt = preis * prozent/100  # <-- Zeile mit Defekt
+    return preis - rabatt
 
 # Manuelle Tests (werden in Baustein 05 durch echte Unit-Tests ersetzt)
 if __name__ == "__main__":
     # TODO: Ergänze mindestens 3 eigene print()-Tests für berechne_rabatt_korrigiert().
     # Überlege selbst: Was sind sinnvolle Eingaben? Was erwartest du als Ergebnis?
+    print(berechne_rabatt_korrigiert(100.0, -10.0)) #Case negatives Prozent behandelt
+    print(berechne_rabatt_korrigiert(100.0, 'a')) #ungültiges Zeichen in Prozent behandelt
+    print(berechne_rabatt(100.0, 120))  # Zu hohe Prozentangabe behandelt
 
-    print("=== Test: berechne_rabatt (fehlerhaft) ===")
-    print(berechne_rabatt(100.0, 20))  # Falsche Ausgabe erwartet
-
-    print("\n=== Test: berechne_rabatt_korrigiert ===")
-    # TODO: Deine Tests hier
 
 
 # ============================================================
